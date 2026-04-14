974bb016618c5dd606f4bdd66c3deef862688973
Zab <znajafian69@gmail.comgit config --global user.name Zabgit commit -m first>
2026-04-13 19:09:33 -0700

    add sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,16 +1,18 @@
-export default function sitemap() {
+import { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://buyiq.app",
+      url: 'https://buyiq.app',
       lastModified: new Date(),
     },
     {
-      url: "https://buyiq.app/product/airpods3",
+      url: 'https://buyiq.app/product/airpods3',
       lastModified: new Date(),
     },
     {
-      url: "https://buyiq.app/product/airpods4",
+      url: 'https://buyiq.app/product/airpods4',
       lastModified: new Date(),
     },
-  ];
+  ]
 }
